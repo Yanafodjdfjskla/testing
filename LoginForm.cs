a4715296bfab681c25463e992c2a69771d76d604
Yanafodjdfjskla <61915001+Yanafodjdfjskla@users.noreply.github.com>
2020-03-07 20:10:54 +0300

    Add files via upload

---
+++ LoginForm.cs
@@ -0,0 +1,78 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testingofpupils
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            this.pass.AutoSize = false;
+            this.pass.Size = new Size(this.pass.Size.Width, 32);
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string loginuser = login.Text;
+            string passuser = pass.Text;
+
+            DB db = new DB();
+
+            DataTable tabel = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `students` WHERE `login`=@lu AND `password`=@pu", db.getConnection());
+            command.Parameters.Add("@lu", MySqlDbType.VarChar).Value = loginuser;
+            command.Parameters.Add("@pu", MySqlDbType.VarChar).Value = passuser;
+           
+
+            adapter.SelectCommand = command;
+            adapter.Fill(tabel);
+
+            if(tabel.Rows.Count>0)
+            {
+                
+                MainForm mf = new MainForm();
+                mf.Show();
+                Hide();
+
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+            
+           
+        }
+
+        private void Login_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testingofpupils
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            teacherForm teacher = new teacherForm();
+            teacher.Show();
+            Hide();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Текст второго вопроса";
+        }
+
+        private void Label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click_2(object sender, EventArgs e)
+        {
+            label2.Text = "Текст второго вопроса";
+        }
+
+        private void Label1_MouseEnter(object sender, EventArgs e)
+        {
+            (sender as Control).ForeColor = Color.FromArgb(106, 177, 135);
+        }
+
+        private void Label1_MouseLeave(object sender, EventArgs e)
+        {
+            (sender as Control).ForeColor = Color.FromArgb(0, 59, 70);
+        }
+
+        private void ПравкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+        }
+
+        private void ToolStripSeparator1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ВыделитьВсёToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            добавитьЗаданиеToolStripMenuItem.Enabled = false;
+        }
+    }
+}
